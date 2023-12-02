39c3c0897914691fc1370e91a63332a07e59492d
Stephan Cook <36123533+PhynnCastro@users.noreply.github.com>
2023-12-02 14:13:55 -0700

    pool.go

--- cmd/karlsenbridge/pool.go
+++ cmd/karlsenbridge/pool.go
@@ -1,5 +1,9 @@
 func connectToPool() {
   conn, err := net.Dial("tcp", config.Pool.Host+":"+config.Pool.Port)
-
   // handle connection  
+}
+
+func submitShare(share string) {
+  _, err := conn.Write([]byte(share))
+  // handle response
 }
